35080d459089a980f0aa7c6ad82bb5a311cf5ffc
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-02-02 06:41:10 -0300

    feat: Add ParticipantInput component

---
+++ src/components/participantInput.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native'
+
+interface IProps {
+  onPressHandler: (newParticipante: string) => void
+}
+
+const ParticipantInput: React.FC<IProps> = ({ onPressHandler }) => {
+  const [nomeParticipante, setNomeParticipante] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+        value={nomeParticipante}
+        onChangeText={setNomeParticipante}
+      />
+      <TouchableOpacity
+        style={styles.btnContainer}
+        onPress={() => onPressHandler(nomeParticipante)}
+      >
+        <Text style={styles.btnText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    gap: 16,
+  },
+  input: {
+    flex: 1,
+
+    height: 56,
+    padding: 16,
+
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+
+    color: '#FFF',
+    fontSize: 16,
+  },
+  btnContainer: {
+    width: 56,
+    height: 56,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    backgroundColor: '#31CF67',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
+})
+
+export default ParticipantInput
